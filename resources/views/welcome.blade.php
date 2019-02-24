eb4317603718414823905871dcfa16875afc9a5b
Yapal123 <46794773+Yapal123@users.noreply.github.com>
2019-02-24 14:15:52 +0300

    last commit probably

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -2,15 +2,15 @@
     @section('content')
      <div class="container-fluid h-100 bg-dark">
          <div class="row h-100  align-items-center">
-             <div class="col-5 m-auto  ">
+             <div class="col-md-5 m-auto col-sm-12 ">
                  <h1 class="text-center text-light">Hello, dear guest</h1>
                  <h3 class="text-center text-light">Before using our site
                 you need log in or sign up</h3><br>
                 <div class="row justify-content-around text-center">
-                    <div class="col-4">
+                    <div class="col-md-4 col-sm-6">                        
                         <a href="{{route('login')}}"><button class="btn btn-success w-75">Log In</button></a>
-                    </div>
-                    <div class="col-4">
+                    </div><br>  <br>  <br>  
+                    <div class="col-md-4 col-sm-6">
                         <a href="{{route('register')}}"><button class="btn btn-success w-75">Sign Up</button></a>
                     </div>
                 </div>
